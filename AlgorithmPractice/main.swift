a6828a8c1818d91a6a94781f0c796bde4b4bc2f3
Nick <605820993@qq.com>
2016-07-06 22:15:44 +0800

    Initial Commit

---
+++ AlgorithmPractice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AlgorithmPractice
+//
+//  Created by administrasion on 7/6/16.
+//  Copyright © 2016 NJU. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
